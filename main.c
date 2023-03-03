ad75eb39da8c7b3ff30829ee0ae5fea542a70cd0
nasamadi <nassrinesamadi@gmail.com>
2023-03-03 04:07:43 +0100

    corr

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <pipex.h>
-
-int	main(int argc, char **argv, char **envp)
-{
-	check_argc(argc);
-	return (fourex(argc, argv, envp));
-}
--- main_bonus.c
+++
@@ -1,7 +0,0 @@
-#include <pipex_bonus.h>
-
-int	main(int argc, char **argv, char **envp)
-{
-	check_argc(argc);
-	return (nex(argc, argv, envp));
-}
